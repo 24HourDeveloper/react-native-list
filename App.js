c8c9dfaaff03bd40fad515b0dcbe19710bdd3e01
Michael Moore <mikeitt009@gmail.com>
2020-04-08 15:52:50 -0400

    displaying a list by mapping an array WRONG

--- App.js
+++ App.js
@@ -2,9 +2,22 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const people = [
+    "Steve Jobs",
+    "Bill Gates",
+    "Katherine Johnson",
+    "Annie Easley",
+    "Mark Zuckerberg",
+    "Linus Torvalds",
+    "Brendan Eich",
+  ];
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={{ marginTop: 30 }}>
+      {people.map((person, index) => (
+        <Text style={{ fontSize: 25, paddingVertical: 40 }} key={index}>
+          {person}
+        </Text>
+      ))}
     </View>
   );
 }
